eb4411938755307cd50f1a96bd7545e2a10eec40
Pumidol <pumidol.l@gmail.com>
2023-08-05 11:53:17 +0700

    ADD challenge

---
+++ challenge-01/calc_area_rectangle.py
@@ -0,0 +1,7 @@
+# Calculate area of a rectangle
+width = input("ความกว้างของสี่เหลี่ยมผืนผ้าเท่าไหร่? (cm) ")
+height = input("ความสูงของสี่เหลี่ยมผืนผ้าเท่าไหร่? (cm) ")
+width = float(width)
+height = float(height)
+area_of_rectangle = width * height
+print("พื้นที่ของสี่เหลี่ยมผืนผ้าคือ " + str(area_of_rectangle) + " ตารางเซนติเมตร")
